f80124e462ec0025533ba9cf4ebb4ec6503e81f5
Riccardo Ressi <11461454+rressi@users.noreply.github.com>
2026-05-13 06:49:50 +0200

    Fix package exports

--- src/is_palindrome/__init__.py
+++ src/is_palindrome/__init__.py
@@ -1,4 +1,4 @@
-from .simpler import solution
+from .simpler import solution as solution_simpler
 from .bfs import solution as solution_bfs
 
-__all__ = ["solution", "solution_bfs"]
+__all__ = ["solution_simpler", "solution_bfs"]
